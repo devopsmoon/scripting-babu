41b562e248a3967498051083192ea31b03d23658
devopsmoon <md_syd01@yahoo.co.in>
2021-04-13 17:56:32 +0530

    modified file1

--- sample.sh
+++ sample.sh
@@ -5,5 +5,5 @@ if rm $name 2>/dev/null
 then
   echo -e "/e[1;32mThe given file $name is delete\e[0m"
   else
-    echo -e "/e[1;33mThe given file $name does not exist\e[0"
+    echo -e "/e[1;33mThe given file $name does not exist\e[0m"
     fi
