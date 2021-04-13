db50067a495ada9dd8a2375ea9af7a37d7309a1b
devopsmoon <md_syd01@yahoo.co.in>
2021-04-13 19:10:09 +0530

    modified file2

--- sample.sh
+++ sample.sh
@@ -3,7 +3,7 @@ echo -n "Enter the file name to delete = "
 read name
 if rm $name 2>/dev/null
 then
-  echo -e "/e[1;32mThe given file $name is delete\e[0m"
+  echo -e "\e[1;32mThe given file $name is delete\e[0m"
   else
-    echo -e "/e[1;33mThe given file $name does not exist\e[0m"
+    echo -e "\e[1;33mThe given file $name does not exist\e[0m"
     fi
